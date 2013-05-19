08f7ae4d386ef1c45f91d54c2b3aa8ad0aa756f1
Matthew Phillips <matthew@matthewphillips.info>
2013-05-19 18:26:15 -0400

    First commit.

---
+++ tasks/webcomponent.js
@@ -0,0 +1,46 @@
+/*
+ * grunt-webcomponent
+ * https://github.com/matthewp/grunt-webcomponent
+ *
+ * Copyright (c) 2013 Matthew Phillips
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs'),
+    pack = require('wcpack');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('webcomponent', 'Your task description goes here.', function() {
+    var done = this.async();
+
+    var data = this.data,
+        options = data.options,
+        file = data.file,
+        outFileName = options && options.out,
+        minify = options && options.minify,
+        indentSize = options && options.indent;
+
+    var packer = pack(file).minify(minify);
+    if(indentSize) {
+      packer.indent(indentSize);
+    }
+
+    packer.end(function(output) {
+      if(outFileName) {
+        fs.writeFile(outFileName, output, 'utf8', function(err) {
+          if(err) throw err;
+
+          done();
+        });
+      }
+    });
+ 
+  });
+
+};
